4947626930934dcc7bec26812cd7891fd7769666
Maykel Arias Torres <hanibal920915@gmail.com>
2022-08-01 11:30:36 -0400

    Corrigiendo el theme

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,14 @@
+import Gitlab from '@geist-ui/react-icons/github'
 export default {
-  gitlab: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Kiero',
+  github: 'https://github.com/m4yk3ldev/doc_kiero',
+  projectLinkIcon: <Gitlab />,
+  docsRepositoryBase: 'https://github.com/m4yk3ldev/doc_kiero',
+  titleSuffix: ' - Kiero CO',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Kiero Co</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Kiero</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        Documentación de Kiero
+        Documentación
       </span>
     </>
   ),
@@ -16,42 +18,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="es_CO" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <title>Documentación de Kiero</title>
     </>
   ),
   search: true,
   prevLinks: true,
   nextLinks: true,
-  footer:false,
+  defaultMenuCollapsed: true,
+  footer: true,
+  footerEditLink: 'Editar en GitLab',
+  footerText: <>MIT {new Date().getFullYear()} © Kiero.</>,
   unstable_faviconGlyph: '📚',
+  unstable_stork: true,
 }
